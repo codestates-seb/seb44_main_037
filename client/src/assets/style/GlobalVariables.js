83b0a7afd11c85eda9bca7fb9e4d78b7fd353686
DanoHwang <danohwang00@gmail.com>
2023-07-05 11:09:06 +0900

    FE/Style: GlobalVariables, 이미지 추가

--- client/src/assets/style/GlobalVariables.js
+++ client/src/assets/style/GlobalVariables.js
@@ -12,6 +12,15 @@ const variables = css`
   --red: #f04646;
   --pink: #f67951;
   --beige: #fbf7f2;
+  --background: #fafafa;
+  --bs-sm: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
+    0 2px 8px hsla(0, 0%, 0%, 0.05);
+  --bs-md: 0 1px 3px hsla(0, 0%, 0%, 0.06), 0 2px 6px hsla(0, 0%, 0%, 0.06),
+    0 3px 8px hsla(0, 0%, 0%, 0.09);
+  --bs-lg: 0 1px 4px hsla(0, 0%, 0%, 0.09), 0 3px 8px hsla(0, 0%, 0%, 0.09),
+    0 4px 13px hsla(0, 0%, 0%, 0.13);
+  --bs-xl: 0 10px 24px hsla(0, 0%, 0%, 0.05), 0 20px 48px hsla(0, 0%, 0%, 0.05),
+    0 1px 4px hsla(0, 0%, 0%, 0.1);
 `;
 
 export default variables;
