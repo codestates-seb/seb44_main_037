75e57b1759076cc0e5e8007e114e2ea7e0632d68
DanoHwang <danohwang00@gmail.com>
2023-07-15 22:49:51 +0900

    BE/Feat: 일반 상품 구매 기능 추가

---
+++ server/routes/controllers/products/buy.js
@@ -0,0 +1,74 @@
+const { INVALID_REQUEST, UNEXPECTED_ERROR, OK, INSUFFICIENT_POINT } = require("../../../constants/messages");
+const Product = require("../../../models/Product");
+const User = require("../../../models/User");
+
+async function buy(req, res, next) {
+  const { productId } = req.body;
+  const user = req.user;
+
+  try {
+    const product = await Product.findOne({ _id: productId });
+
+    if (!productId || !product) {
+      return res
+        .status(400)
+        .send({
+          result: FAILED,
+          message: INVALID_REQUEST
+        });
+    }
+
+    if (!product.isOnSale) {
+      return res
+        .status(400)
+        .send({
+          result: FAILED,
+          message: INVALID_REQUEST
+        });
+    }
+
+    if (user.point < product.price) {
+      return res
+        .status(400)
+        .send({
+          result: FAILED,
+          message: INSUFFICIENT_POINT
+        });
+    }
+    const productFilter = { _id: product._id };
+    const productUpdate = { isOnSale: false };
+
+    const updatedProduct = await Product.findOneAndUpdate(productFilter, productUpdate, { new: true });
+
+    const buyerFilter = { _id: user._id };
+    const buyerUpdate = {
+      point: user.point - product.price,
+      $push: { shoppingList: productId }
+    }
+
+    await User.findOneAndUpdate(buyerFilter, buyerUpdate);
+
+    const sellerFilter = { _id: product.seller._id };
+    const sellerUpdate = {
+      point: user.point + product.price,
+      $push: { salesList: productId }
+    }
+
+    await User.findOneAndUpdate(sellerFilter, sellerUpdate);
+
+    return res
+      .status(200)
+      .send({
+        result: OK,
+        payload: { product: updatedProduct }
+      });
+  } catch (error) {
+    if (error.status) {
+      next(error);
+    }
+
+    next({ message: UNEXPECTED_ERROR });
+  }
+}
+
+module.exports = buy;
--- server/routes/products.js
+++ server/routes/products.js
@@ -9,12 +9,15 @@ const bidInstantly = require("./controllers/products/bidInstantly");
 const getAllProducts = require("./controllers/products/getAllProducts");
 const bid = require("./controllers/products/bid");
 const closeBid = require("./controllers/products/closeBid");
+const buy = require("./controllers/products/buy");
 
 router.get("/", getAllProducts);
 
 router.get("/:id", getProduct);
 
 router.post("/new", multerUpload.array("imagesData"), checkUserInfo, register);
+
+router.post("/buy", checkUserInfo, buy);
 
 router.post("/bid", checkUserInfo, bid);
 
